a7767d6d762964f81b035d71eb500d67b684ebf0
Admin-PC\Admin <pil717171@gmail.com>
2018-10-30 13:20:59 +0300

    modify HM#06

--- 06/main.js
+++ 06/main.js
@@ -2,15 +2,14 @@ var array1 = [123, "string", true, {value: 321}, null],
     array2 = ["first", "second", "last"],
     result = [];
 
+    for (var i = 0; i <array1.length; i++) {
+        result[i] = array1[i];
+    }
+    
+    for (var i = 0; i < array2.length; i++) {
+        result[i + array1.length] = array2[i];
+    }
 
-for (i=0; i < array1.length; i++) {
-    result.push(array1[i]);
-    };
-
-for (i=0; i < array2.length; i++) {
-    result.push(array2[i]);
-};
- 
 console.log("result: ", result);
 
 var result0 =[];
