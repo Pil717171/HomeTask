7b71c01591dd1284f7022da239f2d2cd3190f344
Admin-PC\Admin <pil717171@gmail.com>
2018-12-06 18:58:09 +0300

    HW #17 and mod. HW#15

---
+++ 17/src/script/components/tires.js
@@ -0,0 +1,26 @@
+function drive (element) {
+    element.classList.add("tires-wheel__rotate");    
+};
+
+function drive2 (element) {
+    element.classList.add("tires-images__drive"); 
+};
+
+function coord () {
+    document.querySelectorAll(".tires-images").forEach(function (images) {
+        var coord = images.getBoundingClientRect()
+        if (coord.y < document.documentElement.clientHeight) {
+            drive2(images);
+            document.querySelectorAll(".tires-wheel").forEach(function (wheel) {
+                var coord2 = wheel.getBoundingClientRect()
+                if (coord2.y < document.documentElement.clientHeight) {
+                    drive(wheel);
+                }
+            })    
+        }
+    })
+}
+
+window.onscroll =  coord;
+
+
